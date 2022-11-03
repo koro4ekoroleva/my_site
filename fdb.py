f322e18bbbf00eab22e48e840a21a8cca5c224e8
User <ElizavetkaKuzina@yandex.ru>
2022-11-03 12:59:08 +0300

    подключение бд2

--- fdb.py
+++ fdb.py
@@ -1,8 +1,22 @@
 import sqlite3
 
+from app import app
 
-def connect_bd():
-    conn = sqlite3.connect()
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    '''Вспомогательная функция по созданию БД'''
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+    pass
 
 
 
