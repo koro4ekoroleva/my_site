a927dd055bb05209265da132b26ad0e9c81bcd01
Liza <ElizavetkaKuzina@yandex.ru>
2022-11-10 12:52:00 +0300

    CERF

--- sql_db.sql
+++ sql_db.sql
@@ -1,11 +1,11 @@
 create table if not exists mainmenu(
-id integer primary autoincrement,
+id integer primary key autoincrement,
 title text not null,
 url text not null
-)
+);
 
 create table if not exists recepts(
 id integer primary key autoincrement,
 title text not null,
 recept text not null,
-url text not null)
+url text not null);
